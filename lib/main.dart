3ef0ccd8f9c2ed50e717e1f85925c61d4c7ffaa2
CelineSelke1 <celineselke@outlook.com>
2025-03-01 19:44:16 -0500

    Add feature to add and delete tasks

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,7 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -31,33 +29,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Widget> items = [];
+  List<String> tasks = [];
+  List<bool> checkedTasks = [];  
 
+  void addTask() {
+    setState(() {
+      tasks.add("New Task ${tasks.length + 1}");
+      checkedTasks.add(false); 
+      items.add(
+        SizedBox(
+          width: 200,
+          height: 100,
+          child: Row(children: [
+              Checkbox(
+                value: checkedTasks.last,
+                onChanged: (bool? value) {
+                  setState(() {
+                    checkedTasks[checkedTasks.length - 1] = value!;
+                  });
+                },
+              ),
+              Text(tasks.last),
+              IconButton(onPressed: () {deleteTask(0);}, icon: Icon(Icons.restore_from_trash)),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  void deleteTask(int index){
+    setState(() {
+      tasks.removeAt(index);
+      items.removeAt(index);
+      checkedTasks.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: items,
         ),
       ),
- 
+      floatingActionButton: FloatingActionButton(
+        onPressed: addTask,
+        child: const Text("Add Task"),
+      ),
     );
   }
 }
